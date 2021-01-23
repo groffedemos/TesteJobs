f751d71d3632953277b061be7d991dbe076e0b3a
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-23 08:28:29 -0300

    Update Startup.cs

--- APIFinancas/Startup.cs
+++ APIFinancas/Startup.cs
@@ -21,7 +21,7 @@ namespace APIFinancas
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIFinancas", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIFinancas", Version = "v2" });
             });
 
             if (!string.IsNullOrWhiteSpace(Configuration["ApplicationInsights:InstrumentationKey"]))
